793a8c7d505c0d3ac74aa49ff163382ba206299f
xingchen <2318826148@qq.com>
2022-03-22 11:29:25 +0800

    ANDCJNDJCNJDNVNFD

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	cmd := exec.Command("lotus-miner", "/c", "/root/go/src/new_rust", "--v")
+	cmd := exec.Command("lotus-miner", "--v", "/root/go/src/new_rust")
 	//err := cmd.Run()
 	buf, err := cmd.Output()
 	if err != nil {
