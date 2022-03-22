36c43f3d64238f35a25c38f22a1d8906ff802913
xingchen <2318826148@qq.com>
2022-03-22 11:07:34 +0800

    secenc

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
+	"os"
 )
 
 func main() {
 
-	f, err := exec.Command("/root/go/src/pledge/lotus-miner ", "sealing jobs").Output()
+	shellPath := "/root/go/src/new_rust/lotus-miner"
+	argv := make([]string, 2)
+	argv[0] = "sealing"
+	argv[1] = "jobs"
+	attr := new(os.ProcAttr)
+	newProcess, err := os.StartProcess(shellPath, argv, attr) //运行脚本
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err)
 	}
-	fmt.Println(string(f))
+	fmt.Println("Process PID", newProcess.Pid)
+	processState, err := newProcess.Wait() //等待命令执行完
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("processState PID:", processState.Pid()) //获取PID
+	fmt.Println("ProcessExit:", processState.Exited())   //获取进程是否退出
 }
