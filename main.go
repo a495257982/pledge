0cee8650a081fb7831040dfc20cd3d17af64ae45
xingchen <2318826148@qq.com>
2022-03-22 10:41:43 +0800

    aaa

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	f, err := exec.Command("./lotus-miner ", "sealing jobs").Output()
+	f, err := exec.Command("/root/go/src/pledge/lotus-miner ", "sealing jobs").Output()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
